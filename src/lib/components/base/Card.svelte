a37b9e23fda9d5a2f1ed1f7ab5b1d28f3db2d163
xKesvaL <jabeddou@gmail.com>
2024-01-11 21:20:23 +0100

    feat: add vercel-adapter

--- src/lib/components/base/Card.svelte
+++ src/lib/components/base/Card.svelte
@@ -5,13 +5,18 @@
 	interface Props {
 		class?: string;
 		children?: Snippet;
+		href?: string;
 	}
 
-	const { class: classes, children } = $props<Props>();
+	const { class: classes, children, href } = $props<Props>();
 </script>
 
-<div class={cn('rounded-lg bg-card p-4 shadow-md', classes)}>
+<svelte:element
+	this={href ? 'a' : 'div'}
+	{href}
+	class={cn('rounded-lg bg-card p-4 shadow-md', classes)}
+>
 	{#if children}
 		{@render children()}
 	{/if}
-</div>
+</svelte:element>
